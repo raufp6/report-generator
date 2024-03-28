7402de3d24df2b649852df6df808b5970677fd0b
raufp6coding <raufp6.coding@gmail.com>
2024-03-28 11:41:32 +0530

    host updated

--- core/settings.py
+++ core/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG')
 
-ALLOWED_HOSTS = ["13.234.34.220","localhost","127.0.0.1"]
+ALLOWED_HOSTS = ["13.126.30.188","localhost","127.0.0.1"]
 
 
 # Application definition
